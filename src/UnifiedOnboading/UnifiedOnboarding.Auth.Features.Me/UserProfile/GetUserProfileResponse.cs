13f7a71e7c477179d58aa8a29296d374df88f11a
Sadequzzaman M <sadeq.monoj@gmail.com>
2026-04-24 23:04:35 +0600

    add GetUserProflieRespose Class

---
+++ src/UnifiedOnboading/UnifiedOnboarding.Auth.Features.Me/UserProfile/GetUserProfileResponse.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnifiedOnboarding.Auth.Features.Me.UserProfile;
+
+public record GetUserProfileResponse
+(
+        long UserId, string FirstName, string LastName, string Email, DateTime CreatedAt
+    );
