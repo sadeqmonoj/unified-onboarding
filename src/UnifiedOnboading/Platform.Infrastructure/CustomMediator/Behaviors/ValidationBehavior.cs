391235218bbc5af49953a6d314c85e83f6d6b664
Sadequzzaman M <sadeq.monoj@gmail.com>
2026-04-24 21:36:19 +0600

    Add Proflie response

--- src/UnifiedOnboading/Platform.Infrastructure/CustomMediator/Behaviors/ValidationBehavior.cs
+++ src/UnifiedOnboading/Platform.Infrastructure/CustomMediator/Behaviors/ValidationBehavior.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using FluentValidation;
 using FluentValidation.Results;
 using Platform.BuildingBlocks.CustomMediator;
---
+++ src/UnifiedOnboading/UnifiedOnboarding.Auth.Features.Me/Profile/GetUserProfileResponse.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnifiedOnboarding.Auth.Features.Me.Profile;
+
+public class GetUserProfileResponse
+{
+    public int UserId { get; set; }
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string PhoneNumber { get; set; } = string.Empty;
+    public DateTime DateOfBirth { get; set; }
+}
